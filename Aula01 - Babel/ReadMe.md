66d8dea7bc1ca388af17245f4485fb4e5aefb4fa
Alex Cesar Nascimento <alexcesar.n@gmail.com>
2022-05-31 20:47:36 -0300

    feat(aula01): utilizando babel

---
+++ Aula01 - Babel/ReadMe.md
@@ -0,0 +1,8 @@
+`npm init -y`
+
+`npm i`
+
+`npm install --save-dev @babel/cli @babel/preset-env @babel/core`
+
+`npx babel index.js -o bundle.js --presets=@babel/env`
+
---
+++ Aula01 - Babel/index.js
@@ -0,0 +1,10 @@
+// Babel é um transpilador ele traduz e compila o código para o browser de acordo com o browser que está sendo utilizado.
+// É utilizado para converter o código para navegadores antigos
+
+
+class Pessoa {
+    constructor(nome, sobrenome) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+    }
+}
